4d797a9a6131432cb6c6ef15700e536e206c855f
goodcpu-ksoo <hsnks100@gmail.com>
2017-05-24 03:24:52 +0900

    막 운동

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update:update });
 
 function preload() {
 
@@ -11,49 +11,32 @@ function preload() {
     game.load.image('bullet', 'bullet.png');
 
 }
+var bullet = null;
 
+var lastTime = 0;
 function create() {
 
     //  This creates a simple sprite that is using our loaded image and
     //  displays it on-screen
-    var s = game.add.sprite(80, 0, 'bullet'); 
-    s.rotation = 0.14;
 
-    var s2 = game.add.sprite(0, 0, 'bullet'); 
-    var s3 = game.add.sprite(780, 570, 'bullet'); 
-    var s4 = game.add.sprite(800, 600, 'bullet'); 
-    s4.anchor.x = 0.5;
-    s4.anchor.y = 0.5;
+    bullet = game.add.sprite(400, 300, 'bullet'); 
+    bullet.anchor.x = 0.5;
+    bullet.anchor.y = 0.5; 
+    lastTime = this.game.time.totalElapsedSeconds();
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+function update(){
+    var dt = this.game.time.totalElapsedSeconds() - lastTime;
+    // console.log(dt);
 
 
-    // 일반적
-    function MyObject(){
-        var name = "var name";
-        this.name = "this name"; 
-    };
+    bullet.position.x += getRandomArbitrary(-100, 100) * dt;
+    bullet.position.y += getRandomArbitrary(-100, 100) * dt;
+    lastTime = this.game.time.totalElapsedSeconds();
+    // console.log(this.game.time.elapsed);
+    // console.log(dt._deltaTime);
 
-    var ins = new MyObject();
-    console.log(ins.name);
-
-    var Animal = function(sex, name){
-        if (arguments.length == 1){
-            this.sex = sex;
-        }
-    };
-
-    var Cat = function(name){
-        this.name = name;
-    };
-    Cat.prototype = new Animal();
-
-    var Dog = function(name){
-        this.name = name;
-    };
-    Dog.prototype = new Animal();
-
-    var danbi = new Cat("단비");
-    var sundol = new Dog("순돌");
-
-    console.log(danbi.name);
-    console.log(sundol.name); 
 }
